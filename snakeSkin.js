f73214129547326c810e0b50dd52afccfd8f8dfe
Caleb Ferguson <yonohabloespanol@gmail.com>
2017-09-23 10:21:47 -0400

    Addition of second slider for decay of shapes
    through the shrinkSlider() function.
    
    + minor text edits

--- snakeSkin.js
+++ snakeSkin.js
@@ -4,10 +4,16 @@ var directions; //text directions variables
 
 var squares, sqRadius; // square variables
 var sq1, sq2, sq3, sq4;// auxiliary squares
-var angle, sliderValue;// environment variables
+var angle;// environment variables
 
 var spin_sliderX, spin_sliderY, spin_sliderW, spin_sliderH, spin_sliderStart, spin_sliderEnd, shrink_sliderX, shrink_sliderY, shrink_sliderW, shrink_sliderH, shrink_sliderStart, shrink_sliderEnd;// slider variables
 var spin_sliderValue, shrink_sliderValue;       //variables for mapped value from sliders
+
+//NEW INITS
+
+var decay; // environment variables
+
+//NEW INITS END
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
@@ -21,14 +27,18 @@ function setup(){
     // variable initialization
     squares = []; // [x, y, rotation, radius]
     sqRadius = 100;
+    decay = 0.1;
     
-    Spin_sliderX = windowWidth/20;
-    sliderY = windowHeight/20;
-    sliderW = windowWidth/40;
-    sliderH = windowHeight/60;
-    sliderStart = windowWidth/20;
-    sliderEnd = windowHeight/3;
-    sliderValue = 5;
+// NEW SETUPS
+    shrink_sliderX = windowWidth/30;
+    shrink_sliderY = windowHeight/15;
+    shrink_sliderW = windowWidth/80;
+    shrink_sliderH =windowHeight/25;
+    shrink_sliderStart = windowHeight/12;
+    shrink_sliderEnd = windowHeight/3;
+    shrink_sliderValue = 0.1;
+    
+    //END NEW SETUP
     
     spin_sliderX = windowWidth/20;
     spin_sliderY = windowHeight/20;
@@ -42,8 +52,8 @@ function setup(){
     sq2 = [200, 100, 0.3, sqRadius-20];
     sq3 = [300, 100, 0.4, sqRadius-10];
     sq4 = [400, 100, 0.5, sqRadius];
-    angle = radians(sliderValue/fR);
-    instructions ="INSTRUCTIONS: click and drag the mouse to draw squares. Use the Left and Right arrows to change the spin speed";
+    angle = radians(spin_sliderValue/fR);
+    instructions ="INSTRUCTIONS: click and drag the mouse to draw squares. Use the Up, Down, Left, and Right arrows to change the squares";
 
   squares = [sq1, sq2, sq3, sq4];
 }
@@ -52,6 +62,7 @@ function draw(){
 	background(0);
     showInstructions();
     spinSlider();
+    shrinkSlider();
 	shrinkSquares();
 	spinSquares();
 	drawSquares();
@@ -66,7 +77,6 @@ function showInstructions () {
     text(instructions,windowWidth-windowWidth/8,windowHeight/10,windowWidth/8, 100);
     pop();
 }
-
 //This function is the slider that controls the degree of spinning with the left and right arrows
 function spinSlider() {
 	push();
@@ -89,7 +99,29 @@ function spinSlider() {
     pop();
     angle = radians(spin_sliderValue/fR);
 }
-
+//This function i sthe slider that controls the rate of shape decay with the up and down arrows
+function shrinkSlider() {
+	push();
+    rectMode(CORNER);
+	stroke(255);
+	fill(175);
+  // Keep rectangle within limits of slider
+  shrink_sliderY = constrain(shrink_sliderY, shrink_sliderStart, shrink_sliderEnd-shrink_sliderH);
+  // Draw a line for slider
+  line(shrink_sliderX + shrink_sliderW/2, shrink_sliderStart, shrink_sliderX + shrink_sliderW/2, shrink_sliderEnd);
+  // Draw rectangle for slider
+  rect(shrink_sliderX, shrink_sliderY, shrink_sliderW, shrink_sliderH);
+  // Take the slider's range and map it to a value between 0 and 5
+    shrink_sliderValue = map(shrink_sliderY,shrink_sliderStart,shrink_sliderEnd-shrink_sliderH,0,1);
+    print(shrink_sliderValue);
+	// Update the Slider Value as keys are pressed
+    if  (keyIsDown(DOWN_ARROW)){
+        shrink_sliderY+=5;};
+    if  (keyIsDown(UP_ARROW)){
+        shrink_sliderY-=5;};
+    pop();
+    decay = shrink_sliderValue;
+}
 // This function draws every square contained in the "squares" array
 function drawSquares(){
 	for (var i = 0; i < squares.length; i++) {
@@ -103,7 +135,7 @@ function drawSquares(){
 // This function shrinks the squares and removes them from the array
 function shrinkSquares(){
 	for (var i = 0; i < squares.length; i++) {
-		squares[i][3] -= 0.1;
+		squares[i][3] -= decay;
 		if(squares[i][3] <= 0){
 			squares.splice(i, 1);
 		}
@@ -122,7 +154,8 @@ function createSquare(xpos, ypos, rotation, rad) {
 	// Add it to the array
 	squares.push(newSquare);
 }
-
+// This is the built in mousedrag function
 function mouseDragged(){
 	createSquare(mouseX, mouseY, 0, sqRadius);
 }
+
